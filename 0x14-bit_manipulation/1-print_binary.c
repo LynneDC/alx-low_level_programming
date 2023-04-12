f81e72df17a222a4ae62d9928e8feb7d98c65dfc
LynneDC <roselinedc1308@gmail.com>
2023-04-12 01:17:36 -0700

    Print binary

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -8,19 +8,21 @@
 
 void print_binary(unsigned long int n)
 {
-unsigned long int mask;
+unsigned long int current;
+int i, count = 0;
 
-mask = 1UL << (sizeof(unsigned long int) * 8 - 1);
-while (mask > 0)
+for  (i = 63; i >= 0; i++)
 {
-if (n & mask)
+current = n >> i;
+
+if (current & 1)
 {
-putchar('1');
+_putchar('1');
+count++;
 }
-else
-{
-putchar('0');
+else if (count)
+_putchar('0');
 }
-mask >>= 1;
-}
+if (!count)
+_putchar('0');
 }
