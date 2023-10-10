98a9a14291977b4a1383936b5608662e52c4885b
LynneDc <roselinedc1308@gmail.com>
2023-10-10 15:34:15 +0200

    fix 6 to 9

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -2,16 +2,17 @@
 #include <stdio.h>
 
 /**
- * * linear_search - function that searches for a value in an array of integers
- * * @array: pointer to the first element of the array to search in
- * * @size: number of elements in an array
- * * @value: value to search for
- * * Return: index of value
- * */
+ * linear_search - function that searches for a value in an array of integers
+ * @array: pointer to the first element of the array to search in
+ * @size: number of elements in an array
+ * @value: value to search for
+ * Return: index of value
+ */
 
 int linear_search(int *array, size_t size, int value)
 {
 	size_t i;
+
 	if (array == NULL)
 		return (-1);
 	for (i = 0; i < size; i++)
