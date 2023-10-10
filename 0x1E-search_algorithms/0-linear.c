b579228e2af5b49889fc135536bf3b0fdf3ba57e
LynneDc <roselinedc1308@gmail.com>
2023-10-10 15:26:53 +0200

    searches for a value in an array of integers using linear search

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -16,7 +16,7 @@ if (array == NULL)
 return (-1);
 for (i = 0; i < size; i++)
 {
-printf("Value checked array [%i] = [%li]\n", array[i], i);
+printf("Value checked array [%lu] = [%d]\n", i, array[i]);
 if (array[i] == value)
 return (i);
 }
